a2c0ce68d41be43864a9ea482c0b2c4f3ca837b0
eagle <sadeddinnahsen@gmail.com>
2024-09-26 23:32:38 +0300

    developed the safety

--- client/post-app/src/components/categories/Categories.jsx
+++ client/post-app/src/components/categories/Categories.jsx
@@ -1,26 +1,60 @@
 import React from "react";
+import "./style.css";
+import { Button, Form, Input, Modal } from "antd";
+
+import { useState } from "react";
+
+import { PlusOutlined } from "@ant-design/icons";
 
 const Categories = () => {
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   return (
     <ul className="flex mr-10 overflow-auto md:flex-col text-xl gap-4 ">
-      <li className="bg-green-600 min-w-[145px] text-center transition-all hover:bg-purple-500 text-white cursor-pointer px-6 py-10">
+      <li className="category-item">
         <span>Tümü</span>
       </li>
-      <li className="bg-green-600 min-w-[145px] text-center transition-all hover:bg-purple-500 text-white cursor-pointer px-6 py-10">
+      <li className="category-item">
         <span>Tümü</span>
       </li>
-      <li className="bg-green-600 min-w-[145px] text-center transition-all hover:bg-purple-500 text-white cursor-pointer px-6 py-10">
+      <li className="category-item">
         <span>Tümü</span>
       </li>
-      <li className="bg-green-600 min-w-[145px] text-center transition-all hover:bg-purple-500 text-white cursor-pointer px-6 py-10">
+      <li className="category-item">
         <span>Tümü</span>
       </li>
-      <li className="bg-green-600 min-w-[145px] text-center transition-all hover:bg-purple-500 text-white cursor-pointer px-6 py-10">
+      <li className="category-item">
         <span>Tümü</span>
       </li>
-      <li className="bg-green-600 min-w-[145px] text-center transition-all hover:bg-purple-500 text-white cursor-pointer px-6 py-10">
+      <li className="category-item">
         <span>Tümü</span>
       </li>
+      <li
+        onClick={() => setIsAddModalOpen(true)}
+        className="category-item hover:opacity-90  !bg-purple-800"
+      >
+        <PlusOutlined className="md:text-2xl" />
+      </li>
+      <Modal
+        footer={false}
+        onCancel={() => setIsAddModalOpen(false)}
+        title="Kategori Ekle"
+        open={isAddModalOpen}
+      >
+        <Form layout="vertical">
+          <Form.Item
+            name={"title"}
+            rules={[{ required: true, message: "Bu Alan Zorunlu" }]}
+            label="Kategori Ekle"
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item className="flex justify-end">
+            <Button htmlType="submit" type="primary">
+              Ekle
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </ul>
   );
 };
