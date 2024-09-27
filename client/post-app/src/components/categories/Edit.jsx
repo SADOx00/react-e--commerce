7fd8e1e2711648a802f0fc4730a94639c1325f66
eagle <sadeddinnahsen@gmail.com>
2024-09-27 23:56:38 +0300

    added 'Modals'

--- client/post-app/src/components/categories/Edit.jsx
+++ client/post-app/src/components/categories/Edit.jsx
@@ -1,9 +1,75 @@
-import { Button, Form, Input, Modal, Table } from "antd";
+import { Button, Form, Input, message, Modal, Table } from "antd";
 import FormItem from "antd/es/form/FormItem";
 import { useState } from "react";
 
 const Edit = ({ categ, setCateg, isEditModalOpen, setIsEditModalOpen }) => {
   const [editingRow, setEditingRow] = useState({});
+
+  const handleInputChange = (e) => {
+    setEditingRow({ ...editingRow, title: e.target.value });
+  };
+
+  const handleClick = async (record) => {
+    if (editingRow._id === record._id) {
+      try {
+        const response = await fetch(
+          "http://localhost:5000/api/categories/upd-cat",
+          {
+            method: "PUT",
+            body: JSON.stringify({
+              categoryId: record._id,
+              title: editingRow.title,
+            }),
+            headers: { "Content-type": "application/json; charset = UTF-8" },
+          }
+        );
+
+        if (response.ok) {
+          message.success("Kategori Başarıyla Güncellendi.");
+          setCateg((prev) =>
+            prev.map((cat) =>
+              cat._id === record._id ? { ...cat, title: editingRow.title } : cat
+            )
+          );
+        } else {
+          message.error("Kategori güncellenirken bir hata oluştu.");
+        }
+      } catch (error) {
+        console.log(error);
+        message.error("Sunucuya bağlanırken bir hata oluştu.");
+      } finally {
+        setEditingRow({});
+      }
+    }
+  };
+
+  const handleClick_2 = async (record) => {
+    try {
+      const response = await fetch(
+        "http://localhost:5000/api/categories/del-cat",
+        {
+          method: "DELETE",
+          body: JSON.stringify({
+            categoryId: record._id,
+          }),
+          headers: { "Content-type": "application/json; charset = UTF-8" },
+        }
+      );
+
+      if (response.ok) {
+        message.warning("Kategori Başarıyla Silindi.");
+        setCateg((prev) => prev.filter((cat) => cat._id !== record._id));
+      } else {
+        message.error("Kategori silinirken bir hata oluştu.");
+      }
+    } catch (error) {
+      console.log(error);
+      message.error("Sunucuya bağlanırken bir hata oluştu.");
+    } finally {
+      setEditingRow({});
+    }
+  };
+
   const columns = [
     {
       title: "Category Title",
@@ -11,8 +77,12 @@ const Edit = ({ categ, setCateg, isEditModalOpen, setIsEditModalOpen }) => {
       render: (_, record) => {
         if (record._id === editingRow._id) {
           return (
-            <FormItem>
-              <Input value={record.title}></Input>
+            <FormItem name={"title"}>
+              <Input
+                defaultValue={editingRow.title}
+                value={editingRow.title}
+                onChange={handleInputChange}
+              />
             </FormItem>
           );
         } else {
@@ -29,8 +99,14 @@ const Edit = ({ categ, setCateg, isEditModalOpen, setIsEditModalOpen }) => {
             <Button onClick={() => setEditingRow(record)} type="link">
               Düzenle
             </Button>
-            <Button type="text">Kaydet</Button>
-            <Button danger type="text">
+            <Button
+              onClick={() => handleClick(record)}
+              className="text-green-600"
+              type="text"
+            >
+              Kaydet
+            </Button>
+            <Button onClick={() => handleClick_2(record)} danger type="text">
               Sil
             </Button>
           </div>
@@ -38,6 +114,7 @@ const Edit = ({ categ, setCateg, isEditModalOpen, setIsEditModalOpen }) => {
       },
     },
   ];
+
   return (
     <Modal
       onCancel={() => setIsEditModalOpen(false)}
@@ -46,12 +123,7 @@ const Edit = ({ categ, setCateg, isEditModalOpen, setIsEditModalOpen }) => {
       open={isEditModalOpen}
     >
       <Form>
-        <Table
-          rowKey={"_id"}
-          columns={columns}
-          dataSource={categ}
-          bordered
-        ></Table>
+        <Table rowKey={"_id"} columns={columns} dataSource={categ} bordered />
       </Form>
     </Modal>
   );
